9f63378026aef42fd1ed62e70d39bfb8a6dcd929
ChaymaaLabied <chaymaalabied1996@gmail.com>
2025-10-15 10:37:19 +0200

    Job01

---
+++ Jour01/Job01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My beloved JavaScript</title>
+
+    <script>
+        alert("Hello Javascript!");
+    </script>
+</head>
+
+<body>
+    <h1>Hi JavaScript ! it's been a while .. </h1>
+</body>
+
+</html>
