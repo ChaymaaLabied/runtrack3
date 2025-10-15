015e87713cf98ab9af5cd0eef149a8ddccceb78f
ChaymaaLabied <chaymaalabied1996@gmail.com>
2025-10-15 15:01:14 +0200

    Job06

---
+++ Jour01/Job06/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>FizzBuzz</title>
+</head>
+
+<body>
+
+    <h1>FizzBuzz jusqu'à 151</h1>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
