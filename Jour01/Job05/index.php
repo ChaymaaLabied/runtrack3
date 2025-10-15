b6f2265a737f33dd631deb1bb6799d791682d4aa
ChaymaaLabied <chaymaalabied1996@gmail.com>
2025-10-15 14:42:43 +0200

    Job05

---
+++ Jour01/Job05/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Jours de la semaine</title>
+</head>
+
+<body>
+
+    <h1>Jours de la semaine</h1>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ Jour01/Job05/script.js
@@ -0,0 +1,11 @@
+function afficherjourssemaines() {
+
+  const jourssemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    //jourssemaines.lenght c'est la longueur du tableau , d'une maniere generale , dans noytre cas c 6
+  for (let i = 0; i < jourssemaines.length; i++) {
+
+    // les jours de la semaines sont affichés dans la console 
+    console.log(jourssemaines[i]);
+  }
+}
+afficherjourssemaines();
